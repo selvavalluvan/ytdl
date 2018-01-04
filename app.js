3a76ec25728914cedc6f0fc67e53e77d98378aa1
Selva Valluvan <selva.valluvan@quickmobile.com>
2018-01-04 14:26:52 -0800

    Loading icon

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var path = require('path');
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-require('mongoose').Promise = global.Promise;
+var fs = require('fs');
+var youtubedl = require('youtube-dl');
+var randomstring = require("randomstring");
+var FormData = require('form-data');
+var Joi = require('joi');
 
-var Asset = require('./models/assets');
+var creativeParams = Joi.object().keys({
+    url: Joi.string().required(),
+    brand: Joi.string().required(),
+    tags: Joi.array().required(),
+    orientation: Joi.string().valid('portrait', 'landscape').default('landscape'),
+    floorValue: Joi.number().required(),
+    ceilValue: Joi.number().required()
+});
 
 var app = express();
 
-//Connect Mongoose to MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/ytassets');
-
 // Define the port to run on
-app.set('port', 3000);
+app.set('port', 9000);
 
 //This is how you serve a static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -20,25 +27,82 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.post('/upload', function (req, res) {
-    var data = req.body;
-    console.log(data);
-    //now the password is not encrypted. You have to encrypt.
-    var newAsset = new Asset({
-        name: data.name,
-        brand: data.brand,
-        url: data.url,
-        tags: data['tags[]'],
-        offered:  data.offered,
-        potential: data.potential
-    });
-    newAsset.save().then(function () {
-        res.json({message: 'Asset successfully added!'});
-    }).catch(function (error) {
-        console.error(error);
-        res.json({message: 'Oops! Something went wrong.'});
+    Joi.validate(req.body, creativeParams, function (error, params) {
+        if(error) return res.status(422).json("Missing parameters");
+        try {
+            throw new Error("Booyah!");
+            let video = youtubedl(req.body.url,
+                // Optional arguments passed to youtube-dl.
+                ['--format=18']);
+            let randomName = randomstring.generate(7);
+            let data = params;
+            data.orientation = 'landscape';
+
+// Will be called when the download starts.
+            video.on('info', function (info) {
+                data.name = info.title;
+                data.thumbnail = info.thumbnail;
+                console.log(data)
+            });
+
+            video.pipe(fs.createWriteStream(path.join(__dirname, 'files', randomName + ".mp4")));
+
+            video.on('end', function () {
+                console.log("here");
+                var form = new FormData();
+
+                download(data.thumbnail, randomName + '.jpg', function () {
+                    console.log("here1");
+                    form.append('creative', fs.createReadStream(path.join(__dirname, 'files', randomName + ".mp4")));
+                    form.append('logo', fs.createReadStream(path.join(__dirname, 'files', randomName + ".jpg")));
+                    form.append('thumbnail', fs.createReadStream(path.join(__dirname, 'files', randomName + ".jpg")));
+                    form.append('name', data.name);
+                    form.append('brand', data.brand);
+                    form.append('tags', "[\"" + data["tags[]"].join('","') + "\"]");
+                    form.append('orientation', data.orientation);
+                    form.append('floorValue', data.offered);
+                    form.append('ceilValue', data.potential);
+                    var http = require('http');
+
+                    let headers = Object.assign({}, form.getHeaders(), {'Cookie': 'bzr-enterprise=eyJzaWQiOiI0NmQ1MmUxZi00ZDI2LTQxYTktYTEyZC1jMjU0ZmFkY2UyYWUiLCJ1aWQiOiJlMTk5NzNkMS1iNWRmLTQ4OTYtYTY2Ny0zYTMxNzI1NWRlMzYifQ==;bzr-enterprise.sig=OtchgjqJr4QIh9HWeHlobh03Ii0;'});
+                    console.log(headers);
+                    var request = http.request({
+                        method: 'POST',
+                        hostname: 'localhost',
+                        port:3000,
+                        path: '/client/14/campaign/21/creative',
+                        headers: headers
+                    });
+
+                    form.pipe(request);
+
+                    request.on('response', function(resp) {
+                        console.log(resp.status);
+                        res.json({message: 'Asset successfully added!'});
+                    });
+
+                });
+
+            });
+        } catch (error){
+            return res.status(422).json("Something went wrong! Try again");
+        }
+
     });
 
 });
+
+var download = function(uri, filename, callback){
+    var request = require('request');
+    request.head(uri, function(err, res, body){
+        console.log('content-type:', res.headers['content-type']);
+        console.log('content-length:', res.headers['content-length']);
+
+        request(uri).pipe(fs.createWriteStream(path.join(__dirname, 'files', filename))).on('close', callback);
+    });
+};
+
+
 
 // Listen for requests
 var server = app.listen(app.get('port'), function () {
